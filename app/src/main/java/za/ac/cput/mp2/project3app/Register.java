4786d1da527f7df50afef158e64916671dfb617c
WaseemD <waseem.dollie.wd@gmail.com>
2022-08-15 18:23:18 +0200

    Second Commit Home/Doctor screen

--- app/src/main/java/za/ac/cput/mp2/project3app/Register.java
+++ app/src/main/java/za/ac/cput/mp2/project3app/Register.java
@@ -3,16 +3,32 @@ package za.ac.cput.mp2.project3app;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
+
+    TextView btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        btn1 = findViewById(R.id.btn1);
+
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.action_bar_layout);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent R = new Intent(Register.this, Login.class);
+                startActivity(R);
+                finish();
+            }
+        });
     }
 }
